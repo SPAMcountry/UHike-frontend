0b657ef6e3c1a571238752da24ab69c15741ce8a
mbehi <mohsin.behi@gmail.com>
2021-06-01 16:07:02 -0500

    completed the WeatherCard

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Button';
+
+import LocationForm from './LocationForm';
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+}
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LocationForm />
     </div>
-  );
+  )
 }
 
 export default App;
