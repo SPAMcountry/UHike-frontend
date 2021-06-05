040a22917a74b6ff608b099522b334f956244cf4
sunny-lee3 <sunnylee3712@gmail.com>
2021-06-05 13:26:43 -0700

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
     <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-    redirectUri={process.env.REACT_APP_SERVER}
+    redirectUri={window.location.origin}
     >
     <App />
     </Auth0Provider>
